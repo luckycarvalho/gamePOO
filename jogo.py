0dc986b81e5365955915b3582b1dc5577c06a9f6
luckycarvalho <126518568+luckycarvalho@users.noreply.github.com>
2023-10-20 00:07:54 -0300

    Melhorias no código
    Removi a poluição do código

--- jogo.py
+++ jogo.py
@@ -1,133 +1,77 @@
 from menu import Menu
 from random import randint
-from time import sleep
 class Jogo:
     def __init__(self):
         self.__menu = Menu()
         
-    def escolher_personagens(self):
-        # Escolhendo personagens, jogador/inimigo
-        self.__menu.definir_personagem()
-        while True:
-            print(30*"=")
-            print("Atributos do personagem:")
-            print(30*"=")
-            self.__menu.personagem_jogador.imprimir()
-            print("\n0 - (SIM)\n1 - (NÃO)")
-            confirm = int(input("Deseja escolher outro personagem? "))
-
-            while confirm < 0 or confirm > 1:
-                print("Digite uma opção válida")
-                print("0 - (SIM)\n1 - (NÃO)")
-                confirm = int(input("Deseja escolher outro personagem? "))
-            if confirm == 0:
-                self.__menu.definir_personagem()
-            else:
-                break
-            
-        print()    
-        self.__menu.definir_inimigo()
-        
-         # Caso todos os inimigos sejam mais fortes que o jogador, ele pede para escolher outro        
-        if self.__menu.personagem_inimigo == object:
-            print("Escolha outro personagem")
-            return self.escolher_personagens()
-        
-        while True:
-            print(30*"=")
-            print("Atributos do inimigo:")
-            print(30*"=")
-            self.__menu.personagem_inimigo.imprimir()
-            print("\n0 - (SIM)\n1 - (NÃO)")
-            confirm = int(input("Deseja escolher outro inimigo? "))
-            
-            while confirm < 0 or confirm > 1:
-                print("Digite uma opção válida")
-                print("0 - (SIM)\n1 - (NÃO)")
-                confirm = int(input("Deseja escolher outro inimigo? "))
-            if confirm == 0:
-                self.__menu.definir_inimigo()
-            else:
-                break
-        
-    def escolher_cenarios(self):
-        print()
-        self.__menu.definir_habitat()
+    @property
+    def jogador(self):
+        return self.__jogador
     
-    def lutar(self):
-        jogador = self.__menu.personagem_jogador
-        inimigo = self.__menu.personagem_inimigo
+    @property
+    def inimigo(self):
+        return self.__inimigo
+          
+    def __definir_personagens(self):
+        print(30 * "=")
+        print(5 * " ", "Definindo jogador")
+        print(30 * "=")
+        self.__jogador = self.__menu.escolher_personagem()
+        print(30 * "=")
+        print(5 * " ", "Definindo inimigo:")
+        print(30 * "=")
+        self.__inimigo = self.__menu.escolher_personagem()
+        
+    def __definir_cenario(self):
         print()
-        print(10 * "=", "BATALHA", 10 * "=")
-        # teste de batalha
+        self.__habitat_batalha = self.__menu.definir_habitat()
+        self.__verificar_habitat()
+    
+    def __lutar(self):
+        print(20 * "=")
+        print(5 * " ", "BATALHA")
+        print(20 * "=")
+        # Função que procede a luta caso os personagens estejam vivos, retorna True ou False
+        def lutar_criatura(criatura, adversario, movimento):
+            if criatura.vida > 0:
+                if movimento == "a":
+                    criatura.atacar(adversario)
+                elif movimento == "d":
+                    criatura.defender(adversario)
+            else:
+                return False
+            return True
+        # laço para que o jogador controle a luta
         while True:
-            print("iniciando...")
-            sleep(.5)
-            print("Agora é a sua vez!")
-            print()
-            
-            while jogador.vida > 0 and inimigo.vida > 0:
-                movimento_inimigo = randint(0, 1)
-                print("Digite 'A' para atacar ou 'D' para defender...")
-                movimento_jogador = input("...").lower()
-                   
-                if movimento_jogador == 'd':
-                    print("Você decidiu defender!")
-                    sleep(1)
-                    jogador.defender(inimigo) 
-                    print("...")
-                    sleep(1) 
-                elif movimento_jogador == 'a': 
-                    print("Você decidiu atacar!")
-                    sleep(1) 
-                    jogador.atacar(inimigo)
-                    print("...")
-                    sleep(1)
-                else:
-                    while True:
-                        print("Opção inválida!")
-                        movimento_jogador = input("...").lower()
-                        if movimento_jogador == 'a' or movimento_jogador == 'd':
-                            break
-                    
-                if jogador.vida == 0 or inimigo.vida == 0:
-                    break
-        
-                if movimento_inimigo == 0:
-                    print(f"{inimigo.nome} decidiu atacar!")
-                    sleep(1)
-                    inimigo.atacar(jogador)
-                    print("...")
-                    sleep(1)
-                else:
-                    print(f"{inimigo.nome} decidiu defender!")
-                    sleep(1)
-                    inimigo.defender(jogador)
-                    print("...")
-                    sleep(1)
-                       
-            break  
-        
-        if jogador.vida == 0:
-            print()
-            sleep(1)
-            print("Você perdeu!")
-            print()
-            sleep(1)
-        elif inimigo.vida == 0:
-            print()
-            sleep(1)
-            print(f"{inimigo.nome} perdeu!")
-            print()
-            sleep(1)
-            
-        print(f"Informações do jogador:")
-        jogador.imprimir()
-        print()
-        print("Informações do inimigo:") 
-        inimigo.imprimir()
-        
+            movimento_jogador = input("Digite 'A' para atacar ou 'D' para defender: ").lower()
+            if movimento_jogador in ["a", "d"]:
+                jogador_vivo = lutar_criatura(self.jogador, self.inimigo, movimento_jogador)
+            else:
+                raise ValueError("Digite um valor válido.")
+
+            movimento_inimigo = "a" if randint(0, 1) == 0 else "d"
+            inimigo_vivo = lutar_criatura(self.inimigo, self.jogador, movimento_inimigo)
+
+            if not jogador_vivo:
+                print("Você perdeu!")
+                break
+            elif not inimigo_vivo:
+                print("O inimigo perdeu!")
+                break
+     
+    def __verificar_habitat(self):
+        #Aumenta os pontos caso o habitat de algum dos personagens seja igual ao do cenário de batalha
+        if self.jogador.habitat == self.__habitat_batalha:
+            self.jogador.vida += 10
+            self.jogador.forca += 5
+            self.jogador.defesa += 10
+        elif self.inimigo == self.__habitat_batalha: 
+            self.inimigo.vida += 10
+            self.inimigo.forca += 5   
+            self.inimigo.defesa += 10
+                
+                  
     def iniciar(self):
-        self.escolher_personagens()
-        self.escolher_cenarios()
-        self.lutar()
+       self.__definir_personagens()
+       self.__definir_cenario()
+       self.__lutar()
--- menu.py
+++ menu.py
@@ -2,102 +2,38 @@ from monstro import monstros, Monstro
 from animal import animais, Animal
 class Menu:
     def __init__(self):
-        self.personagens = ["Animal", "Monstro"]
+        self.personagens = {"Animal": animais, "Monstro": monstros}
         self.cenarios = ["Floresta", "Caverna"]
-        self._personagem_jogador = object
-        self._personagem_inimigo = object
         
-    @property
-    def personagem_jogador(self):
-        return self._personagem_jogador
-    
-    @property
-    def personagem_inimigo(self):
-        return self._personagem_inimigo
-    
-    @property
-    def habitat_batalha(self):
-        return self._habitat_batalha
-    
-    def definir_personagem(self):
-        print("Escolha uma criatura para jogar")
-        #Laço usado constantemente para percorrer uma lista
-        for key, value in enumerate(self.personagens):
-           print(f"{key} - {value}")
-                   
-        num_criatura = int(input("Digite o número da criatura: "))
-        #validação
-        while num_criatura < 0 or num_criatura > (len(self.personagens) - 1):
-            print("Digite um número válido")
-            num_criatura = int(input("Digite o número da criatura: "))
-        # verifica se o jogador escolheu o tipo animal    
-        if self.personagens[num_criatura] == "Animal": 
-            #percorre e exibe a lista de personagens do tipo animal disponível
-            for i in range(len(animais)):
-                print(f"{i} - {animais[i].nome}")
-        
-            num_personagem = int(input("Digite o número do personagem: "))
-            #validação
-            while num_personagem < 0 or num_personagem > (len(animais) - 1):
-                 print("Digite um número válido")
-                 num_personagem = int(input("Digite o número do personagem: "))
-         
-            self._personagem_jogador = animais[num_personagem]     
-        #verifica se o jogador escolheu o tipo monstro    
-        elif self.personagens[num_criatura] == "Monstro":
-            #percorre e exibe a lista de personagens do tipo monstro disponível
-            for i in range(len(monstros)):
-                print(f"{i} - {monstros[i].nome}")
-                
-            num_personagem = int(input("Digite o número do personagem: "))
-            #validação
-            while num_personagem < 0 or num_personagem > (len(monstros) - 1):
-                 print("Digite um número válido")
-                 num_personagem = int(input("Digite o número do personagem: "))
-                 
-            self._personagem_jogador = monstros[num_personagem]
-        
-    def definir_inimigo(self):
-        print("Escolha uma criatura como inimigo: ")
-        
-        for key, value in enumerate(self.personagens):
-            print(f"{key} - {value}")
-        
-        criatura_inimiga = int(input("Digite o número da criatura: "))
-        # validação
-        while criatura_inimiga < 0 or criatura_inimiga > (len(self.personagens) - 1):
-            print("Digite um número válido")
-            criatura_inimiga = int(input("Digite o número da criatura: "))
-        
-        if self.personagens[criatura_inimiga] == "Animal":
-            personagem_impresso = False
-            for i in range(len(animais)):
-                # Não permite escolher a si mesmo como inimigo
-                if animais[i] != self._personagem_jogador:
-                    # Verifica se os inimigos são 2x mais fortes que o jogador
-                    if (self.personagem_jogador.calcular_nivel() * 2) >= animais[i].calcular_nivel():  
-                        print(f"{i} - {animais[i].nome}")
-                        personagem_impresso = True
-            #Caso todos os inimigos sejam mais fortes
-            if not personagem_impresso:
-                print("Todos os animais são muito fortes!")
-            else:
-                num_inimigo = int(input("Digite o número do personagem inimigo: "))
-                self._personagem_inimigo = animais[num_inimigo]
+    def escolher_personagem(self):
+        # essa função exibe a lista de personagens disponíveis    
+        def personagens(tipo):
+            # O parâmetro "tipo" armazena a chave/key do dicionário em self.personagens
+            print(f"Escolha um {tipo}")
+            # essa variável armazena os valores da chave/key do dicionário
+            personagens_disponiveis = self.personagens[tipo]
             
-        elif self.personagens[criatura_inimiga] == "Monstro":
-            personagem_impresso = False
-            for i in range(len(monstros)): # Aqui o mesmo processo é repetido
-                if monstros[i] != self._personagem_jogador:
-                    if (self.personagem_jogador.calcular_nivel() * 2) >= monstros[i].calcular_nivel(): 
-                        print(f"{i} - {monstros[i].nome}") 
-                        personagem_impresso = True
-            if not personagem_impresso:
-                print("Todos os monstros são muito fortes!")
-            else:
-                num_inimigo = int(input("Digite o número do personagem inimigo: "))      
-                self._personagem_inimigo = monstros[num_inimigo]
-                
+            for i in range(len(personagens_disponiveis)):
+                print(f"{i} - {personagens_disponiveis[i].nome}")
+            
+            num_personagem = int(input(f"Digite o número do {tipo}: "))
+            
+            if num_personagem < 0 or num_personagem >= len(personagens_disponiveis):
+                raise ValueError("Digite um número válido")
+            
+            return personagens_disponiveis[num_personagem] 
+        # define o tipo de criatura: Animal ou Monstro
+        print("Escolha uma criatura para jogar")
+        for key in self.personagens.keys():
+            print(key)
+        
+        tipo_criatura = input("Digite o tipo da criatura: ")
+        
+        if tipo_criatura not in self.personagens:
+            raise ValueError("Digite um tipo válido")
+        
+        return personagens(tipo_criatura)
+             
     def definir_habitat(self):
         
         print("Escolha um cenário para a batalha")
@@ -110,17 +46,7 @@ class Menu:
         while num < 0 or num > (len(self.cenarios) - 1):
             print("Digite um número válido")
             num = int(input("Digite o número do cenário: "))
-        self._habitat_batalha = self.cenarios[num]
-        self.__verificar_habitat()
+        return self.cenarios[num]
+        
     
-    def __verificar_habitat(self):
-        #Aumenta os pontos caso o habitat de algum dos personagens seja igual ao do cenário de batalha
-        if self.personagem_jogador.habitat == self.habitat_batalha:
-            self._personagem_jogador.vida += 10
-            self._personagem_jogador.forca += 5
-            self._personagem_jogador.defesa += 10
-        elif self.personagem_inimigo == self.habitat_batalha: 
-            self._personagem_inimigo.vida += 10
-            self._personagem_inimigo.forca += 5   
-            self._personagem_inimigo.defesa += 10
-            
+   
